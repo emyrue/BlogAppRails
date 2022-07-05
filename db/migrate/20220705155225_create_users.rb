e26a2cd29d445399c3bdb3a5ad768006c5b12aa6
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-05 14:18:42 -0600

    Fixed foreign key issue

--- db/migrate/20220705155225_create_users.rb
+++ db/migrate/20220705155225_create_users.rb
@@ -8,9 +8,5 @@ class CreateUsers < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
-
-    add_foreign_key :comments, :users, column: :author_id
-    add_foreign_key :posts, :users, column: :author_id
-    add_foreign_key :likes, :users, column: :author_id
   end
 end
