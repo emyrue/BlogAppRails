1156ae3c7c08a15c7a722379606f3d295be34800
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-06 10:01:41 -0600

    Post methods created

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,11 @@ class Post < ActiveRecord::Base
   belongs_to :users, :class_name => 'User', optional: true
 
   def update_post_counter
+    counter = Post.where(author_id: author_id).count
+    User.update(self.author_id, posts_counter: counter)
   end
 
   def most_recent_comments
+    Comment.where(post_id: id).order(created_at: :desc).limit(5)
   end
 end
