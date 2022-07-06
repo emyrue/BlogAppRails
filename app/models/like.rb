f416d535744d38b2609b8bb023891c6ddb8d27b4
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-06 10:09:14 -0600

    Made likes counter method

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,5 +3,7 @@ class Like < ActiveRecord::Base
   belongs_to :posts, :class_name => 'Post', optional: true
 
   def update_like_counter
+    counter = Like.where(post_id: post_id).count
+    Post.update(self.post_id, likes_counter: counter)
   end
 end
