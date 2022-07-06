b3d6ea668fdc4dcb24a973b985456892ea29fa13
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-06 10:05:35 -0600

    Made update comment counter method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :posts, :class_name => 'Post', optional: true
 
   def update_comment_counter
+    counter = Comment.where(post_id: post_id).count
+    Post.update(self.post_id, comments_counter: counter)
   end
 end
