9331bc24d8e331bb87bfb548e8797d6cea596bc4
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-06 09:46:01 -0600

    Most recent posts function created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :posts, :class_name => 'Post'
   has_many :likes, :class_name => 'Like'
 
-  def most_recent_posts(user_id)
-    Post.where(author_id: user_id).order(created_at: :desc).limit(3)
+  def most_recent_posts
+    Post.where(author_id: id).order(created_at: :desc).limit(3)
   end
 end
