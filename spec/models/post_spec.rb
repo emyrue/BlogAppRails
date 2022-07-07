e9ce3f40ad74cfa251731b8026467a9f6ea79965
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-07 10:19:57 -0600

    Post validation tests finished

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,4 +16,34 @@ RSpec.describe Post, type: :model do
     @post = Post.first
     expect(@user.posts_counter).to eq 1
   end
+
+  it "Title should be present" do
+    @post.title = nil
+    expect(@post).to_not be_valid
+  end
+
+  it "Title should be less than or equal to 250 characters" do
+    @post.title = 'a' * 300
+    expect(@post).to_not be_valid
+  end
+
+  it "Comments counter should be an integer" do
+    @post.comments_counter = 1.5
+    expect(@post).to_not be_valid
+  end
+
+  it "Comments counter should be greater than or equal to 0" do
+    @post.comments_counter = -1
+    expect(@post).to_not be_valid
+  end
+
+  it "Likes counter should be an integer" do
+    @post.likes_counter = 1.5
+    expect(@post).to_not be_valid
+  end
+
+  it "Likes counter should be greater than or equal to 0" do
+    @post.likes_counter = -1
+    expect(@post).to_not be_valid
+  end
 end
