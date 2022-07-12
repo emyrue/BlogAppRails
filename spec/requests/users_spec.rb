7b35a80732fa32c6ac231fa5f4f54a5cd29bf137
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-12 09:20:12 -0600

    Testing response status and index template

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get users_path }
+    
+    it "Successfully gets index" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "Renders index template" do
+      expect(response).to render_template('index')
+    end
   end
 end
