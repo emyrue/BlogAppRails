09d611df3b31983cf866045d8adb73b2658007e4
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-13 10:56:31 -0600

    Basic components of posts index completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @all_posts = Post.where(author_id: params[:user_id]).order(created_at: :desc)
-    @limit = 3
   end
 
   def show
