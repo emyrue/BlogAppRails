070185a39a3ae94e3a4843835b66dc8d36c9e154
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-14 15:00:30 -0600

    Added like feature

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+  def create
+    find_post
+    unless already_liked?
+      @like = Like.new(author_id: current_user.id, post_id: @post.id)
+      @like.save
+      redirect_to "/users/#{params[:user_id]}/posts/#{params[:post_id]}"
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(author_id: current_user.id, post_id: @post.id).exists?
+  end
+end
