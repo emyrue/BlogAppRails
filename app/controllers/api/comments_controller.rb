8ff9dd415a72a73b55004d4522be956b135ee11e
emyrue <94935633+emyrue@users.noreply.github.com>
2022-07-20 16:27:53 -0600

    Made create method in comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,4 +3,19 @@ class Api::CommentsController < ApplicationController
     @post = Post.includes(:comments).find(params[:post_id])
     render :json => @post.comments
   end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      puts "saved"
+    else
+      puts "not saved"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author_id, :post_id, :text)
+  end
 end
